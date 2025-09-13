eb4e8b60937124d1913ecc973af1123bac22ae2d
Amrapali Dhobe <amrapalidhobe@Amrapalis-MacBook-Air.local>
2025-09-13 18:35:02 +0530

    Fix Supabase environment variables for Render

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 
-dotenv.config();
-
+// Use Render environment variables
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
